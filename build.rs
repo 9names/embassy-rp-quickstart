e9e82cdfc1f40399ea5ed78339bb7a311e9a94dc
9names <60134748+9names@users.noreply.github.com>
2025-02-03 21:25:39 +1100

    Initial test with RP2350

--- build.rs
+++ build.rs
@@ -31,6 +31,5 @@ fn main() {
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,28 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::block::ImageDef;
 use embassy_rp::gpio;
 use embassy_time::{Duration, Timer};
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
+
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: ImageDef = ImageDef::secure_exe();
+
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"Blinky Example"),
+    embassy_rp::binary_info::rp_program_description!(
+        c"This example tests the RP Pico on board LED, connected to gpio 25"
+    ),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
